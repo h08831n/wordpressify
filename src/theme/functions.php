c064a1a9c546eadf105f31dcfe84d9ac2b898751
hossein <hosseinnaghneh@live.com>
2020-01-06 17:09:29 +0330

    -- update package json and gulp

--- src/theme/functions.php
+++ src/theme/functions.php
@@ -2,12 +2,12 @@
 include 'libs/main-functions.php';
 include 'libs/user-actions.php';
 //include 'libs/jDateTime.php';
+
 function wordpressify_resources() {
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
 	wp_enqueue_script( 'header_js', get_template_directory_uri() . '/js/header-bundle.js', null, 1.0, false );
 	wp_enqueue_script( 'footer_js', get_template_directory_uri() . '/js/footer-bundle.js', null, 1.0, true );
 }
-
 add_action( 'wp_enqueue_scripts', 'wordpressify_resources' );
 
 // Theme setup
